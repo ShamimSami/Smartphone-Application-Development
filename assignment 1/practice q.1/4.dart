4ee62398f10d872f8db23a3b82e7febccf67568e
Shahrear Shamim Sami <shahrearshamimsami26@gmail.com>
2025-10-13 23:33:34 +0600

    commit name

---
+++ assignment 1/practice q.1/4.dart
@@ -0,0 +1,9 @@
+void main() {
+  double p = 1000; // Principal amount
+  double t = 2; // Time in years
+  double r = 5; // Rate of interest
+
+  double simpleInterest = (p * t * r) / 100;
+
+  print('Simple Interest = $simpleInterest');
+}
