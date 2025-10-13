ff0c5472488c923b2ea2acc31e4bd483fbc25007
Shahrear Shamim Sami <shahrearshamimsami26@gmail.com>
2025-10-13 23:46:11 +0600

    commit name

--- assignment 1/practice q.1/7.dart
+++ assignment 1/practice q.1/7.dart
@@ -1,13 +1,15 @@
 import 'dart:io';
 
 void main() {
-  stdout.write('Enter your first name: ');
-  String firstName = stdin.readLineSync()!;
+  stdout.write('Enter the dividend: ');
+  int dividend = int.parse(stdin.readLineSync()!);
 
-  stdout.write('Enter your last name: ');
-  String lastName = stdin.readLineSync()!;
+  stdout.write('Enter the divisor: ');
+  int divisor = int.parse(stdin.readLineSync()!);
 
-  String fullName = '$firstName $lastName';
+  int quotient = dividend ~/ divisor; // integer division
+  int remainder = dividend % divisor; // modulus operator
 
-  print('Your full name is: $fullName');
+  print('Quotient: $quotient');
+  print('Remainder: $remainder');
 }
